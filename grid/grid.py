3b5095c51bf4841ec29b87492a731643d0a611cc
nate-adkins <nathanpadkins@gmail.com>
2025-07-16 14:09:14 -0400

    bleh bye 2 hrs today, 3 yesterday, 5 total

--- grid/grid.py
+++ grid/grid.py
@@ -1,45 +1,79 @@
 import matplotlib.pyplot as plt
-import matplotlib.colors as col
+from matplotlib.colors import ListedColormap, NoNorm 
 import numpy as np
 import random, math
 
 SIZE = 10
-DIMENSIONS = 2 
 
-CLEAR = 0
-OBJECT = 1
-START = 2
-GOAL = 3
+class GridType:
+    CLEAR = 0
+    OBJECT = 1
+    START = 2
+    END = 3
+    VISITED = 4
+    PATH = 5
 
-colors = ["white","black","green","red"]
-cmap = col.ListedColormap(colors)
+COLORS = [  "white",  # clear
+            "black",  # object
+            "blue",   # start
+            "red",    # end
+            "green",  # visited
+            "gray",   # path
+        ]
+
 grid = np.zeros((SIZE, SIZE), dtype=int)
 
-def gaussian_islandz(count = 3):
+def make_rand_coord() -> tuple[int]:
+    return random.randint(0,SIZE-1), random.randint(0,SIZE-1)
+
+def gaussian_islands(count) -> None:
     for _ in range(count):
-        coord = [0,0]
-        for dim in range(DIMENSIONS):
-            coord[dim] = random.randint(0,SIZE-1)
-
-        curr_x, curr_y = coord[0],coord[1]
-        grid[curr_x,curr_y] = OBJECT
-
+        curr_x, curr_y = make_rand_coord()
+        grid[curr_x, curr_y] = GridType.OBJECT
         gaussian_fill(curr_x, curr_y, 0.5)
 
-def gaussian_fill(x,y, prob):
+def gaussian_fill(x,y, prob) -> None:
     cutoff = 0.3
     attenuation  = 0.3
     if prob <= cutoff: return
     for op in [[-1,1],[0,1],[1,1],[-1,0],[1,0],[-1,-1],[0,-1],[-1,1]]:
         new_x, new_y = x+op[0], y+op[1]
         if (0 <= new_x < SIZE and 0 <= new_y < SIZE):
-            grid[new_x,new_y] = OBJECT if random.random() <= (prob if (op[0] == 0 or op[1] == 0) else prob * math.sqrt(2)) else CLEAR      
+            grid[new_x,new_y] = GridType.OBJECT if random.random() <= (prob if (op[0] == 0 or op[1] == 0) else prob * math.sqrt(2)) else GridType.CLEAR      
             gaussian_fill(new_x, new_y, prob * attenuation)
 
+def place_start_end(randomize = False) -> None:
+    
+    for type, coord in zip([GridType.START,GridType.END],[[0,0],[SIZE-1,SIZE-1]]):
+        x, y = coord if not randomize else make_rand_coord()
+        grid[x][y] = type
+
+def ant_alg(start,end):
+    
+    x_diff = end[0] - start[0]
+    y_diff = end[1] - start[1]
+
+    if x_diff == 0 or y_diff == 0: return
+
+    new_x, new_y = tuple[start]
+
+    if x_diff < 0: # go left
+
+        ant_alg
+    elif x_diff > 0: # go right
+
+
+
 def main():
-    gaussian_islandz()
+
+    gaussian_islands(3)
+    place_start_end()
+
     plt.figure(figsize=(10,10))
-    plt.imshow(grid, cmap, interpolation='none', origin='lower')
+
+    cmap = ListedColormap(COLORS)
+    plt.imshow(grid, cmap, norm=NoNorm(), origin='lower')
+
     plt.show()
 
 if __name__ == '__main__':
